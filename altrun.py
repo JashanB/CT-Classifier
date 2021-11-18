a28bd9df46f7bc08605bbe79d2563779a5693ab9
JashanB <jashan_brar50@hotmail.com>
2021-11-17 21:42:18 -0500

    uploading content

---
+++ altrun.py
@@ -0,0 +1,7 @@
+import zipfile
+
+with zipfile.ZipFile("CT-0.zip", "r") as z_fp:
+    z_fp.extractall("./MosMedData/")
+
+with zipfile.ZipFile("CT-23.zip", "r") as z_fp:
+    z_fp.extractall("./MosMedData/")
